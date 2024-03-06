bc62d58ec69a7d81e1486833d8b32320ecaa58b2
ZRE <chy853@gmail.com>
2024-03-07 01:27:52 +0800

    Add Tile class to Dungeon namespace

---
+++ include/Dungeon/Tile.h
@@ -0,0 +1,30 @@
+#ifndef TILE_HPP
+#define TILE_HPP
+
+#include <string>
+
+#include "Dungeon/Elements.h"
+#include "Util/GameObject.hpp"
+#include "Util/SpriteSheet.hpp"
+
+namespace Dungeon {
+
+class Tile : public Util::GameObject {
+public:
+    Tile(const tile &u_Tile);
+
+    void SetIndex(std::size_t index);
+    std::size_t GetIndex();
+
+    void Update();
+
+private:
+    tile m_Tile;
+    glm::vec2 m_TileSize;
+    std::string m_filepath;
+    std::shared_ptr<Util::SpriteSheet> m_SpriteSheet;
+    std::size_t m_Index = 0;
+};
+} // namespace Dungeon
+
+#endif // TILE_HPP
---
+++ src/Dungeon/Tile.cpp
@@ -0,0 +1,47 @@
+#include "Dungeon/Tile.h"
+#include "ToolBoxs.h"
+
+namespace Dungeon {
+
+Tile::Tile(const tile &u_Tile)
+    : m_Tile(u_Tile) {
+    m_filepath =
+        (ASSETS_DIR "/level/") + DUNGEON_TILETYPES.at(m_Tile.type) + ".png";
+    if (m_Tile.type >= 100) {
+        SetZIndex(1);
+    }
+    if (m_Tile.type >= 112 && m_Tile.type <= 117) {
+        m_filepath = (ASSETS_DIR "necrodancer_stage.png");
+    }
+    else if (m_Tile.type == 118) {
+        m_filepath = (ASSETS_DIR "door_front.png");
+    }
+    m_SpriteSheet = std::make_shared<Util::SpriteSheet>(m_filepath);
+    Update();
+}
+
+void Tile::SetIndex(std::size_t index) {
+    m_Index = index;
+    Update();
+}
+
+std::size_t Tile::GetIndex() {
+    return m_Index;
+}
+
+void Tile::Update() {
+    m_TileSize = DUNGEON_TILESIZES.at(m_Tile.type);
+    auto ImgSize =
+        ToolBoxs::CountImagePixel(m_filepath, m_TileSize.x, m_TileSize.y);
+    m_Transform.scale = {DUNGEON_SCALE, DUNGEON_SCALE};
+    m_Transform.translation = {
+        -(m_Tile.x * DUNGEON_TILE_WIDTH * DUNGEON_SCALE),
+        -(m_Tile.y * DUNGEON_TILE_WIDTH * DUNGEON_SCALE)};
+
+    m_SpriteSheet->SetDrawRect(
+        {static_cast<int>(ImgSize.x * m_Index), static_cast<int>(ImgSize.y * 0),
+         static_cast<int>(DUNGEON_TILE_WIDTH), static_cast<int>(ImgSize.y)});
+    m_Drawable = m_SpriteSheet;
+}
+
+} // namespace Dungeon
