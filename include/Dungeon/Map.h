1778ff4dcc4becb703f4eb06b10f013ed4e65a0a
ZRE <chy853@gmail.com>
2024-03-07 01:27:29 +0800

    Add child management functionality to Map class

--- include/Dungeon/Map.h
+++ include/Dungeon/Map.h
@@ -2,8 +2,7 @@
 #define MAP_H
 
 #include "Dungeon/Level.h"
-
-#include <memory>
+#include "Dungeon/Tile.h"
 
 namespace Dungeon {
 
@@ -11,8 +10,33 @@ class Map {
 public:
     Map(const std::string path, const int levelNum = 1);
 
+    /**
+     * @brief Add a child to Renderer.
+     *
+     * @param child The GameObject needing to be managed by Renderer.
+     */
+    void AddChild(const std::shared_ptr<Util::GameObject> &child);
+
+    /**
+     * @brief Add children to Renderer.
+     *
+     * @param children The GameObjects needing to be managed by Renderer.
+     */
+    void
+    AddChildren(const std::vector<std::shared_ptr<Util::GameObject>> &children);
+
+    /**
+     * @brief Remove the child.
+     *
+     * @param child The GameObject being removed.
+     */
+    void RemoveChild(std::shared_ptr<Util::GameObject> child);
+
+    std::vector<std::shared_ptr<Util::GameObject>> GetChildren();
+
 private:
     std::unique_ptr<Level> m_Level;
+    std::vector<std::shared_ptr<Util::GameObject>> m_Children;
 };
 
 } // namespace Dungeon
--- src/Dungeon/Map.cpp
+++ src/Dungeon/Map.cpp
@@ -4,6 +4,27 @@ namespace Dungeon {
 
 Map::Map(const std::string path, const int levelNum) {
     m_Level = std::make_unique<Level>(path, levelNum);
+    for (auto &tile : m_Level->GetTiles()) {
+        m_Children.push_back(std::make_shared<Tile>(tile));
+    }
+}
+
+void Map::AddChild(const std::shared_ptr<Util::GameObject> &child) {
+    m_Children.push_back(child);
+}
+
+void Map::AddChildren(
+    const std::vector<std::shared_ptr<Util::GameObject>> &children) {
+    m_Children.insert(m_Children.end(), children.begin(), children.end());
+}
+
+void Map::RemoveChild(std::shared_ptr<Util::GameObject> child) {
+    m_Children.erase(std::remove(m_Children.begin(), m_Children.end(), child),
+                     m_Children.end());
+}
+
+std::vector<std::shared_ptr<Util::GameObject>> Map::GetChildren() {
+    return m_Children;
 }
 
 } // namespace Dungeon
