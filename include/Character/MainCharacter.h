9770d9c53681dc0bc21ca66e724311f937804f63
ZRE <chy853@gmail.com>
2024-03-22 10:27:12 +0800

    Update ZIndex for MainCharacter and Tile

--- include/Character/MainCharacter.h
+++ include/Character/MainCharacter.h
@@ -23,7 +23,7 @@ private:
 
     glm::vec2 InitPosition = {0, 0};
 
-    uint16_t ZIndex = 50;
+    float ZIndex = 0.5;
 
     uint16_t m_start_move_animation_frame = 0;
 
@@ -35,7 +35,6 @@ public:
     [[nodiscard]] const std::vector<std::shared_ptr<Util::GameObject>>
     GetGameElement() const;
 
-
     /* ---- Setter and Getter ----- */
 public:
     [[nodiscard]] uint16_t GetStartMoveFrame() const {
@@ -46,6 +45,8 @@ public:
         return m_Head->GetPosition();
     };
 
+    [[nodiscard]] glm::vec2 GetGamePosition();
+
     void SetPosition(const glm::vec2 &position);
 
     void SetBodyImagePath(const std::string &Path) { m_BodyImagePath = Path; };
@@ -53,6 +54,14 @@ public:
     void SetHeadImagePath(const std::string &Path) { m_HeadImagePath = Path; }
 
     void SetFaceTo(Direction direction);
+
+    void SetZIndex(float index) {
+        ZIndex = index;
+        m_Body->SetZIndex(index);
+        m_Head->SetZIndex(index + float(0.25));
+    }
+
+    void Update() { SetZIndex(GetGamePosition().y + float(0.5)); }
 };
 
 #endif // FUCK_PTSD_MAINCHARACTER_H
