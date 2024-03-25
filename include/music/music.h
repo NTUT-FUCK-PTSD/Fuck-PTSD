25a70e3402e7f470de9d41f34d29c97ceec403ae
onon1101 <mingzhuguly@gmail.com>
2024-03-25 11:32:23 +0800

    music syste

--- include/music/music.h
+++ include/music/music.h
@@ -5,13 +5,35 @@
 #ifndef FUCK_PTSD_MUSIC_H
 #define FUCK_PTSD_MUSIC_H
 
-namespace music {
-    void test();
+#include <iostream>
 
-    class MusicSystem final{
-    public:
+class MusicSystem final {
+public:
+    explicit MusicSystem();
 
-    };
-}
+    ~MusicSystem();
+
+    // the time that start game.
+    // e.g. Time::GetElapsedTimeMs()
+    void updateTime(std::size_t time);
+
+    void playMusic(const std::string path);
+
+    void setMusicSpeed();
+
+protected:
+    // some functional to use
+    std::vector<std::size_t> readTempoFile();
+
+private:
+    // start time that playing music.
+    std::size_t startTimeMs = 0;
+
+    // during time (ms), that from start playing music to now.
+    std::size_t duringTimeMs = 0;
+
+    // music playing speed
+    float m_CurrentMusicSpeed = 0.0f;
+};
 
 #endif // FUCK_PTSD_MUSIC_H
