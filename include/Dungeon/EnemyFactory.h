acbbf11c732c2d19135b30c4312ab4959218f7c5
ZRE <chy853@gmail.com>
2024-03-31 19:42:28 +0800

    Refactor EnemyFactory to use switch case for creating enemy objects

--- include/Dungeon/EnemyFactory.h
+++ include/Dungeon/EnemyFactory.h
@@ -13,25 +13,24 @@ public:
     static std::shared_ptr<Enemy>
     CreateEnemy(const s_Enemy &u_Enemy,
                 const std::shared_ptr<SimpleMapData> &simpleMapData) {
-        // switch (u_Enemy.type) {
-        // case 0:
-        //     return std::make_shared<Enemies::GreenSlime>(u_Enemy);
-        // case 1:
-        //     return std::make_shared<Enemies::BlueSlime>(u_Enemy);
-        // case 2:
-        //     return std::make_shared<Enemies::OrangeSlime>(u_Enemy);
-        // case 3:
-        // case 4:
-        // case 5:
-        // case 6:
-        //     return std::make_shared<Enemies::Bat>(u_Enemy);
-        // case 7:
-        //     return std::make_shared<Enemies::RedBat>(u_Enemy);
+        switch (u_Enemy.type) {
+        case 0:
+            return std::make_shared<Enemies::GreenSlime>(u_Enemy,
+                                                         simpleMapData);
+        case 1:
+            return std::make_shared<Enemies::BlueSlime>(u_Enemy, simpleMapData);
+        case 2:
+            return std::make_shared<Enemies::OrangeSlime>(u_Enemy,
+                                                          simpleMapData);
+        case 6:
+            return std::make_shared<Enemies::Bat>(u_Enemy, simpleMapData);
+        case 7:
+            return std::make_shared<Enemies::RedBat>(u_Enemy, simpleMapData);
 
-        // default:
-        //     return nullptr;
-        // }
-        return std::make_shared<Enemies::GreenSlime>(u_Enemy, simpleMapData);
+        default:
+            return std::make_shared<Enemies::GreenSlime>(u_Enemy,
+                                                         simpleMapData);
+        }
     }
 };
 } // namespace Dungeon
