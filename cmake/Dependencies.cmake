6dd4aacf2d989c5f4b8466fd3f5a32c35d258daf
ZRE <chy853@gmail.com>
2024-04-05 01:57:26 +0800

    Bye Corrosion

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,11 +1,11 @@
 include(FetchContent)
 
-FetchContent_Declare(
-        Corrosion
-        GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
-        GIT_TAG master
-)
-FetchContent_MakeAvailable(Corrosion)
+# FetchContent_Declare(
+#         Corrosion
+#         GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
+#         GIT_TAG master
+# )
+# FetchContent_MakeAvailable(Corrosion)
 
 message(STATUS "Tinyxml2 NOT FOUND, fetching from source!")
 FetchContent_Declare(
