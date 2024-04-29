04df26cf70a56451ca78743a020cd6c9daf3cab1
onon1101 <mingzhuguly@gmail.com>
2024-04-29 17:56:07 +0800

    need to fix bomb

--- include/Settings/Helper.hpp
+++ include/Settings/Helper.hpp
@@ -8,31 +8,16 @@
 #include <array>
 #include <cstddef>
 #include <exception>
-
 #include <glm/vec2.hpp>
 
-<<<<<<< HEAD
-#include <exception>
-=======
->>>>>>> main
 #include "Dungeon/Map.h"
 
 namespace Settings {
 class Helper {
 public:
-<<<<<<< HEAD
     static void Init(Dungeon::Map* dungeonMap);
 
     static std::size_t GamePosToMapIdx(glm::ivec2 gamePos);
-=======
-    static void Init(Dungeon::Map* dungeonMap) { m_DungeonMap = dungeonMap; }
-
-    static std::size_t GamePosToMapIdx(const glm::ivec2 gamePos) {
-        return (gamePos.x - Dungeon::Map::GetLevelIndexMin().x + 1)
-               + (gamePos.y - Dungeon::Map::GetLevelIndexMin().y + 1)
-                     * Dungeon::Map::GetSize().x;
-    };
->>>>>>> main
 
     struct PlayerPos {
         glm::vec2   GamePos;
@@ -40,7 +25,6 @@ public:
     };
     static PlayerPos GetPlayerPosDM();
 
-<<<<<<< HEAD
     static glm::ivec2 Direct2MI(Player::Direction direction);
 
     static glm::vec2 CountImgPixel(
@@ -48,21 +32,6 @@ public:
         std::size_t        widthNumber,
         std::size_t        heightNumber
     );
-=======
-        return PlayerPos{playerPos, mapIdx};
-    };
-
-    inline static glm::ivec2 Direct2MI(Player::Direction direction) {
-        switch (direction) {
-        case Player::Direction::UP: return {0, -1};
-        case Player::Direction::LEFT: return {-1, 0};
-        case Player::Direction::DOWN: return {0, 1};
-        case Player::Direction::RIGHT: return {1, 0};
-        case Player::Direction::NONE:
-            throw std::runtime_error("direction can not be none");
-        }
-    }
->>>>>>> main
 
 private:
     static Dungeon::Map* m_DungeonMap;
