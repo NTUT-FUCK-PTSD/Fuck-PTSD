304f4f2003b3e4a57291d6c31fc292f4d4f8947c
zre <chy853@gmail.com>
2024-05-03 04:11:08 +0800

    Refactor to use eventmaker

--- include/Event/EventArgs.h
+++ include/Event/EventArgs.h
@@ -2,6 +2,7 @@
 #define EVENT_ARGS_H
 
 #include <glm/glm.hpp>
+#include "eventpp/utilities/eventmaker.h"
 
 #include "Event/EventType.h"
 
@@ -18,28 +19,18 @@ private:
     EventType type;
 };
 
-class EnemyMoveEventArgs : public EventArgs {
-public:
-    explicit EnemyMoveEventArgs(const std::size_t mapIndex)
-        : EventArgs(EventType::EnemyMove),
-          m_MapIndex(mapIndex) {}
+EVENTPP_MAKE_EVENT(
+    EnemyMoveEventArgs,
+    EventArgs,
+    EventType::EnemyMove,
+    (std::size_t, GetMapIndex)
+);
 
-    std::size_t GetMapIndex() const { return m_MapIndex; }
-
-private:
-    std::size_t m_MapIndex;
-};
-
-class AttackPlayerEventArgs : public EventArgs {
-public:
-    explicit AttackPlayerEventArgs(const std::size_t damage)
-        : EventArgs(EventType::AttackPlayer),
-          m_Damage(damage) {}
-
-    std::size_t GetDamage() const { return m_Damage; }
-
-private:
-    std::size_t m_Damage;
-};
+EVENTPP_MAKE_EVENT(
+    AttackPlayerEventArgs,
+    EventArgs,
+    EventType::AttackPlayer,
+    (std::size_t, GetDamage)
+);
 
 #endif  // EVENT_ARGS_H
