ab69bf7c88dcb198911415b502f2ec42cf5d92ad
onon1101 <mingzhuguly@gmail.com>
2024-05-13 13:24:21 +0800

    beat

--- include/Music/Music_config.h
+++ include/Music/Music_config.h
@@ -6,6 +6,15 @@ namespace Music::Config {
 static constexpr std::string_view IMAGE_BLUE_INDICATOR = ASSETS_DIR
     "/gui/TEMP_beat_marker.png";
 
-}
+static constexpr std::string_view IMAGE_GREEN_INDICATOR = ASSETS_DIR
+    "/gui/TEMP_beat_marker_green.png";
+
+static constexpr std::string_view IMAGE_RED_INDICATOR = ASSETS_DIR
+    "/gui/TEMP_beat_marker_red.png";
+
+static constexpr std::string_view IMAGE_TARGET_INDICATOR = ASSETS_DIR
+    "/gui/TEMP_beat_target.png";
+
+}  // namespace Music::Config
 
 #endif  // FUCK_PTSD_2024_05_13_MUSIC_CONFIG_H
