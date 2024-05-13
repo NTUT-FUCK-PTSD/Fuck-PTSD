b570e08d90a21d8bb82d8eb54519eac2a0cf0b81
onon1101 <mingzhuguly@gmail.com>
2024-05-13 11:57:41 +0800

    Keeping modify Music's indicator

---
+++ include/Music/IndicatorBar.h
@@ -0,0 +1,84 @@
+#ifndef FUCK_PTSD_2024_05_13_INDICATORBAR_H
+#define FUCK_PTSD_2024_05_13_INDICATORBAR_H
+
+#include <Util/Image.hpp>
+#include <memory>
+#include "UGameElement.h"
+#include "Util/Time.hpp"
+
+namespace Music {
+class IndicatorBar final {
+public:
+    static void Init();
+
+    static void Pause(bool state);
+
+    static std::shared_ptr<Util::GameElement> GetGameElement();
+
+    static void Update();
+
+private:
+    using ms_t = std::size_t;
+    static constexpr ms_t       TEMPO_TIME_RANGE = 2000;
+    static constexpr ms_t       FRAME_UPDATE_MAX = 100;
+    static constexpr glm::ivec2 m_CenterPos = {0, -310};
+
+    static void CountTempoNumber();
+    static void CountIndicatorIntervalSpace();
+
+    static void CreateImage();
+    static void CreateIndicatorLeft();
+    static void CreateIndicatorRight();
+
+    static void UpdateTimer();
+    static void UpdateIndicatorLeft(
+        std::size_t index,
+        std::size_t intervalTime,
+        std::size_t intervalPixel
+    );
+    static void UpdateIndicatorRight(
+        std::size_t index,
+        std::size_t intervalTime,
+        std::size_t intervalPixel
+    );
+
+    static std::size_t m_TempoNumber;
+
+    static std::size_t m_BeatIndex;
+
+    /**
+     * @details This value is the last time the record was updated.
+     * @name m_LastRecordTime
+     */
+    static std::size_t m_LastRecordTime;
+
+    /**
+     * @details This value is the time of the interval time between each
+     * indicator.
+     * @name m_IndicatorIntervalTime
+     */
+    static std::size_t m_IIT;
+
+    static bool m_IsPause;
+
+    /**
+     * @details This value is the number of interval space between each beat
+     * indicator.
+     * @name m_IndicatorIntervalSpace
+     */
+    static std::size_t m_IIS;
+
+    /**
+     * @details The vector of left indicator.
+     */
+    static std::vector<std::shared_ptr<Util::GameElement>> m_IndicatLeft;
+
+    static std::vector<std::shared_ptr<Util::GameElement>> m_IndicatRight;
+
+    static std::shared_ptr<Util::Image> m_IndicatorBlue;
+
+    static std::shared_ptr<Util::GameElement> m_GameElement;
+};
+}  // namespace Music
+
+#endif  // FUCK_PTSD_2024_05_13_INDICATORBAR_H
